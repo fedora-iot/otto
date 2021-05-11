c22e179287f7462f52ce653ef057085cceee8e1b
Tom Gundersen <teg@jklm.no>
2021-05-11 22:43:19 +0100

    tools: add prepare-source.sh
    Tool to tidy and prepare the sources.
    
    Signed-off-by: Tom Gundersen <teg@jklm.no>

---
+++ tools/prepare-source.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -eux
+
+GO_VERSION=1.14.14
+GO_BINARY=$(go env GOPATH)/bin/go$GO_VERSION
+
+# this is the official way to get a different version of golang
+# see https://golang.org/doc/install#extra_versions
+go get golang.org/dl/go$GO_VERSION
+$GO_BINARY download
+
+# ensure that go.mod and go.sum are up to date, ...
+$GO_BINARY mod tidy
+
+# ... the code is formatted correctly, ...
+$GO_BINARY fmt ./...
+
+# ... and all code has been regenerated from its sources.
+$GO_BINARY generate ./...
