0bb8fc6970764201cde615d734f203c1b026ebf6
Christian Kellner <christian@kellner.me>
2021-05-11 22:40:07 +0000

    registry: extract registry related code
    Move the registry related code into its own struct and file. The
    start to disentangle the spaghetti.

--- main.go
+++ main.go
@@ -15,12 +15,10 @@ import (
 	"strconv"
 	"strings"
 
-	"crypto/sha256"
 	_ "crypto/sha512"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/google/uuid"
 	digest "github.com/opencontainers/go-digest"
 	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 )
@@ -96,30 +94,23 @@ func parseRange(s string, size int64) ([]httpRange, error) {
 
 type Server struct {
 	root string
+
+	oci *Registry
 }
 
-func (server *Server) PathForBlob(d digest.Digest) string {
-	return filepath.Join(server.root, "blobs", d.Algorithm().String(), d.Hex())
+func NewServer(root string) *Server {
+	return &Server{
+		root: root,
+		oci:  NewRegistry(filepath.Join(root, "oci")),
+	}
 }
 
-func (server *Server) OpenBlob(d digest.Digest, info *os.FileInfo) (*os.File, error) {
-	blob := server.PathForBlob(d)
-
-	fd, err := os.Open(blob)
+func (server *Server) Init() error {
+	err := server.oci.Init()
 	if err != nil {
-		return nil, err
+		return fmt.Errorf("failed to init registry: %v", err)
 	}
-
-	if info == nil {
-		return fd, nil
-	}
-
-	*info, err = fd.Stat()
-	if err != nil {
-		return nil, err
-	}
-
-	return fd, nil
+	return nil
 }
 
 func MustParseDigest(raw string, w http.ResponseWriter) digest.Digest {
@@ -152,7 +143,7 @@ func (server *Server) HeadBlob(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("repo: '%s', digest: '%s'\n", repo, d.String())
 
 	var fi os.FileInfo
-	_, err := server.OpenBlob(d, &fi)
+	_, err := server.oci.OpenBlob(d, &fi)
 	if err != nil {
 		if os.IsNotExist(err) {
 			http.Error(w, "Blob does not exist", 404)
@@ -181,7 +172,7 @@ func (server *Server) GetBlob(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("repo: '%s', digest: '%s'\n", repo, d.String())
 
 	var fi os.FileInfo
-	fd, err := server.OpenBlob(d, &fi)
+	fd, err := server.oci.OpenBlob(d, &fi)
 	if err != nil {
 		if os.IsNotExist(err) {
 			http.Error(w, "Blob does not exist", 404)
@@ -202,33 +193,18 @@ func (server *Server) GetBlob(w http.ResponseWriter, r *http.Request) {
 func (server *Server) BeginUpload(w http.ResponseWriter, r *http.Request) {
 	repo := chi.URLParam(r, "repo")
 
-	fmt.Printf("upload repo: '%s'\n", repo)
-
-	id := uuid.New()
-
-	uploads := filepath.Join(server.root, "uploads")
-	err := os.MkdirAll(uploads, 0700)
+	uid, err := server.oci.BeginBlob()
 	if err != nil {
 		http.Error(w, err.Error(), 500)
+		return
 	}
-
-	dest := filepath.Join(uploads, id.String())
-	f, err := os.Create(dest)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-	}
-	defer f.Close()
 
 	rh := httpRange{0, 0}
 
-	for k, v := range r.Header {
-		fmt.Printf("HDR %s: %s\n", k, v)
-	}
-
-	w.Header().Set("Location", fmt.Sprintf("/v2/%s/blobs/uploads/%s", repo, id.String()))
+	w.Header().Set("Location", fmt.Sprintf("/v2/%s/blobs/uploads/%s", repo, uid))
 	w.Header().Set("Content-Range", rh.contentRange(0))
 	w.Header().Set("Content-Type", "application/octet-stream")
-	w.Header().Set("Docker-Upload-UUID", id.String())
+	w.Header().Set("Docker-Upload-UUID", uid)
 
 	w.WriteHeader(202)
 }
@@ -237,15 +213,10 @@ func (server *Server) UploadChunked(w http.ResponseWriter, r *http.Request) {
 	uid := chi.URLParam(r, "uuid")
 	repo := chi.URLParam(r, "repo")
 
-	dest := filepath.Join(server.root, "uploads", uid)
+	var fi os.FileInfo
 
-	file, err := os.OpenFile(dest, os.O_WRONLY, 0644)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
+	fd, err := server.oci.ResumeBlob(uid, &fi)
 
-	fi, err := file.Stat()
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
@@ -256,14 +227,14 @@ func (server *Server) UploadChunked(w http.ResponseWriter, r *http.Request) {
 
 		ranges, err := parseRange(rawRange, fi.Size())
 		if err != nil || len(ranges) != 1 {
-			http.Error(w, "Invalid range header", 416)
+			http.Error(w, "Invalid range header", http.StatusRequestedRangeNotSatisfiable)
 			return
 		}
 
-		file.Seek(ranges[0].start, 0)
+		fd.Seek(ranges[0].start, 0)
 	}
 
-	n, err := io.CopyN(file, r.Body, r.ContentLength)
+	n, err := io.CopyN(fd, r.Body, r.ContentLength)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
@@ -271,7 +242,7 @@ func (server *Server) UploadChunked(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Printf("Wrote %d (%d) bytes to %s\n", n, r.ContentLength, uid)
 
-	fi, err = file.Stat()
+	fi, err = fd.Stat()
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
@@ -289,60 +260,19 @@ func (server *Server) UploadFinish(w http.ResponseWriter, r *http.Request) {
 	uid := chi.URLParam(r, "uuid")
 	repo := chi.URLParam(r, "repo")
 
-	dest := filepath.Join(server.root, "uploads", uid)
-
-	file, err := os.Open(dest)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	_, err = file.Stat()
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
 	rawDigest := r.URL.Query().Get("digest")
 	checksum := MustParseDigest(rawDigest, w)
 	if checksum == "" {
 		return
 	}
 
-	fmt.Printf("digest: '%s'\n", checksum.Hex())
-
-	have, err := checksum.Algorithm().FromReader(file)
-
+	checksum, err := server.oci.FinishBlob(uid, checksum)
 	if err != nil {
-		http.Error(w, http.StatusText(500), 500)
+		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	if have != checksum {
-		http.Error(w, "Checksum mismatch", http.StatusBadRequest)
-		return
-	}
-
-	file.Seek(0, 0)
-
-	hasher := sha256.New()
-	if _, err := io.Copy(hasher, file); err != nil {
-		http.Error(w, http.StatusText(500), 500)
-		return
-	}
-
-	d := digest.NewDigest("sha256", hasher)
-
-	blobs := filepath.Join(server.root, "blobs", "sha256")
-	err = os.MkdirAll(blobs, 0770)
-	if err != nil {
-		http.Error(w, http.StatusText(500), 500)
-		return
-	}
-
-	os.Rename(dest, filepath.Join(blobs, d.Hex()))
-
-	w.Header().Set("Location", fmt.Sprintf("/v2/%s/blobs/%s", repo, d.String()))
+	w.Header().Set("Location", fmt.Sprintf("/v2/%s/blobs/%s", repo, checksum.String()))
 	w.WriteHeader(201)
 }
 
@@ -390,43 +320,11 @@ func (server *Server) UploadManifest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	id := uuid.New()
-
-	uploads := filepath.Join(server.root, "uploads")
-	err = os.MkdirAll(uploads, 0700)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-	}
-
-	dest := filepath.Join(uploads, id.String())
-	file, err := os.Create(dest)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-	}
-	defer file.Close()
-
-	data, _ := json.MarshalIndent(m, "", "    ")
-
-	fmt.Printf("%s", data)
-
-	d := digest.FromBytes(data)
-
-	_, err = file.Write(data)
-
+	d, err := server.oci.PutManifest(m)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
-
-	err = file.Close()
-
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	target := filepath.Join(server.root, "blobs", d.Algorithm().String(), d.Hex())
-	os.Rename(dest, target)
 
 	cid, err := server.ImportCommit(commit)
 	if err != nil {
@@ -444,7 +342,7 @@ func (server *Server) UploadManifest(w http.ResponseWriter, r *http.Request) {
 
 func (server *Server) ImportCommit(ci CommitInfo) (string, error) {
 
-	blob := server.PathForBlob(ci.layer)
+	blob := server.oci.PathForBlob(ci.layer)
 
 	tmp, err := ioutil.TempDir(server.root, ".import-*")
 	if err != nil {
@@ -521,10 +419,15 @@ func main() {
 		w.Write([]byte("nothing to see here"))
 	})
 
-	server := Server{root: "/tmp/otto"}
+	server := NewServer("/tmp/otto")
+	err := server.Init()
+
+	if err != nil {
+		log.Fatalf("Failed to initialize server: %v", err)
+	}
 
 	repo := filepath.Join(server.root, "ostree", "repo")
-	err := os.MkdirAll(repo, 0700)
+	err = os.MkdirAll(repo, 0700)
 	if err != nil {
 		log.Fatalf("Failed to create dir: %v", err)
 	}
---
+++ registry.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/google/uuid"
+	digest "github.com/opencontainers/go-digest"
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
+)
+
+type Registry struct {
+	Path string
+
+	hash  digest.Algorithm
+	blobs string
+}
+
+func NewRegistry(path string) *Registry {
+	reg := Registry{
+		Path: path,
+		hash: digest.Canonical,
+	}
+	return &reg
+}
+
+func (reg *Registry) Init() error {
+	err := os.MkdirAll(reg.Path, 0700)
+	if err != nil {
+		return err
+	}
+
+	reg.blobs = filepath.Join(reg.Path, "blobs", string(reg.hash))
+
+	err = os.MkdirAll(reg.blobs, 0700)
+	if err != nil {
+		return err
+	}
+
+	incoming := filepath.Join(reg.Path, "incoming")
+	err = os.MkdirAll(incoming, 0700)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (reg *Registry) PathForBlob(d digest.Digest) string {
+	return filepath.Join(reg.Path, "blobs", d.Algorithm().String(), d.Hex())
+}
+
+func (reg *Registry) OpenBlob(d digest.Digest, info *os.FileInfo) (*os.File, error) {
+	blob := reg.PathForBlob(d)
+
+	fd, err := os.Open(blob)
+	if err != nil {
+		return nil, err
+	}
+
+	if info == nil {
+		return fd, nil
+	}
+
+	*info, err = fd.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	return fd, nil
+}
+
+func (reg *Registry) BeginBlob() (string, error) {
+	uid := uuid.New().String()
+	dest := filepath.Join(reg.Path, "incoming", uid)
+
+	fd, err := os.Create(dest)
+	if err != nil {
+		return "", err
+	}
+	fd.Close()
+
+	return uid, nil
+}
+
+func (reg *Registry) ResumeBlob(uid string, info *os.FileInfo) (*os.File, error) {
+	dest := filepath.Join(reg.Path, "incoming", uid)
+
+	fd, err := os.OpenFile(dest, os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	if info == nil {
+		return fd, nil
+	}
+
+	*info, err = fd.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	return fd, nil
+}
+
+func (reg *Registry) FinishBlob(uid string, verify digest.Digest) (digest.Digest, error) {
+	dest := filepath.Join(reg.Path, "incoming", uid)
+
+	fd, err := os.Open(dest)
+	if err != nil {
+		return "", err
+	}
+
+	_, err = fd.Stat()
+	if err != nil {
+		return "", err
+	}
+
+	checksum, err := verify.Algorithm().FromReader(fd)
+	if err != nil {
+		return "", err
+	}
+
+	if checksum != verify {
+		return "", fmt.Errorf("checksum mismatch: '%s'", checksum.String())
+	}
+
+	if verify.Algorithm() != reg.hash {
+		fd.Seek(0, 0)
+		checksum, err = reg.hash.FromReader(fd)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	target := reg.PathForBlob(checksum)
+	err = os.Rename(dest, target)
+	if err != nil {
+		return "", err
+	}
+
+	return checksum, nil
+}
+
+func (reg *Registry) PutManifest(manifest v1.Manifest) (digest.Digest, error) {
+	data, _ := json.MarshalIndent(manifest, "", "    ")
+
+	checksum := reg.hash.FromBytes(data)
+
+	tmp, err := ioutil.TempDir(reg.Path, ".manifest-*")
+	if err != nil {
+		return "", err
+	}
+	defer os.RemoveAll(tmp)
+
+	dest := filepath.Join(tmp, checksum.String())
+	file, err := os.Create(dest)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	_, err = file.Write(data)
+
+	if err != nil {
+		return "", err
+	}
+
+	err = file.Close()
+
+	if err != nil {
+		return "", err
+	}
+
+	algorithm := checksum.Algorithm().String()
+	hexdigest := checksum.Hex()
+
+	target := filepath.Join(reg.Path, "blobs", algorithm, hexdigest)
+	err = os.Rename(dest, target)
+
+	if err != nil {
+		return "", err
+	}
+
+	return checksum, nil
+}
